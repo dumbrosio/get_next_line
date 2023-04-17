f3f8cf000e1c1d36935d2b47523cd19754f45dae
dxdumbrosio <vd-ambro@student.42roma.it>
2023-04-17 15:54:35 +0200

    split main and gnl

--- get_next_line.c
+++ get_next_line.c
@@ -1,40 +1,21 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: vd-ambro <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/14 15:48:34 by vd-ambro          #+#    #+#             */
-/*   Updated: 2023/04/14 18:17:15 by vd-ambro         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <errno.h>
 
-int main()
+char	*get_next_line(int fd)
 {
-	int		fd;
-	char	*buf;
+	char		*buf;
+	//static char	*tmp = NULL;
+	//char		*src;
+	//char		*dst;
+	//ssize_t		n;
+	
+	buf = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!buf)
+		return (NULL);
+	read(fd, buf, BUFFER_SIZE);
 
-	buf = malloc(sizeof(char) * BUFFER_SIZE + 1);
-	fd = open("test.txt", O_RDONLY);
-	
-	if (fd == -1)
-	{
-		perror("open");
-		exit(EXIT_FAILURE);
-	}
-	
-	printf("opened\n");
-		read(fd, buf, BUFFER_SIZE);
-	printf("%s\n", buf);
-		close(fd);
-	printf("closed");
-	
-	return 0;
+	return (buf);
 }
---
+++ main.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vd-ambro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/14 15:48:34 by vd-ambro          #+#    #+#             */
+/*   Updated: 2023/04/14 18:17:15 by vd-ambro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+
+char	*get_next_line(int fd);
+
+int	main(void)
+{
+	int	fd;
+	char	*line;
+	
+	fd = open("./test.txt", O_RDONLY);
+	if (fd == -1)
+	{
+		perror("open");
+		exit(EXIT_FAILURE);
+	}
+	
+	printf("opened\n");
+	
+	line = get_next_line(fd);
+	
+	printf("%s\n", line);
+	
+	free(line);
+
+	close(fd);
+	
+	printf("closed\n");
+
+	return (0);
+}
